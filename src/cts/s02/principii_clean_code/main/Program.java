033f5b4982342a2addd19dd5240799eac7357598
unknown <ioan.hibrys81@gmail.com>
2023-03-15 09:09:48 +0200

    sem2

---
+++ src/cts/s02/principii_clean_code/main/Program.java
@@ -0,0 +1,25 @@
+package cts.s02.principii_clean_code.main;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import cts.s02.principii_clean_code.clase.Aplicant;
+import cts.s02.principii_clean_code.clase.readers.AngajatReader;
+import cts.s02.principii_clean_code.clase.readers.AplicantReader;
+
+public class Program {
+
+	public static void main(String[] args) {
+		List<Aplicant> listaAngajati;
+		try {
+			AplicantReader reader=new AngajatReader();
+			listaAngajati = reader.readAplicants("angajati.txt");
+			for(Aplicant angajat:listaAngajati)
+				System.out.println(angajat.toString());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
